75e8c2a0fed414e549ade1d0ba64ca2fc868c681
Ariunjargal Ochirpurev <ariunjargal.ochirpurev@gmail.com>
2025-04-22 06:55:53 +0800

    first

---
+++ src/app/(register)/signup/page.tsx
@@ -0,0 +1,8 @@
+export default function SignUpPage() {
+  return (
+    <div className="w-full">
+      <div className="w-1/2 bg-amber-400"></div>
+      <div className="w-1/2 bg-white"></div>
+    </div>
+  );
+}
